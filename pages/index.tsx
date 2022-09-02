e1409a649da8a83097b07f59848eab2e5ae01c30
George Bockari <bitwhys@gmail.com>
2022-09-02 05:05:52 -0400

    refactor(Hero): playing round with design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,7 @@
 import Link from 'next/link'
 import Layout from '@/components/Layout'
+import HeroSection from '@/components/HeroSection'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-5xl font-black">Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => <HeroSection />
 
 export default IndexPage
